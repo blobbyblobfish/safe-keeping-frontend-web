75e0f7c1202998a101c8d58819cbf32ed8ad30c2
blobbyblobfish <aywang6@gmail.com>
2020-08-15 23:00:04 -0400

    Dynamically rendering Line and Scatter groups to the page (dynamic rendering not working)

--- src/components/UserTracker.js
+++ src/components/UserTracker.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { VictoryVoronoiContainer, VictoryLine, VictoryScatter, VictoryTooltip, VictoryGroup, VictoryChart } from 'victory'
+import UserVictoryGroup from './UserVictoryGroup'
 
 function UserTracker({ user }) {
 
-    function diaryDatesArray() {
-        return user.diary_cards.filter(dc => !!dc.diary_card_trackers[0] > 0).map(dc => {
-            const dateObj = new Date(dc.entry_timestamp)
-            const date = dateObj.toISOString().slice(0, 10)
-            return {x: date, y: dc.diary_card_trackers[0].score}
-        })
-    }
+    //Get array of objects where {x: date, y: score} from diary cards.
+    const diaryDatesArray = user.diary_cards.filter(dc => !!dc.diary_card_trackers[0] > 0).map(dc => {
+        const dateObj = new Date(dc.entry_timestamp)
+        const date = dateObj.toISOString().slice(0, 10)
+        return {x: date, y: dc.diary_card_trackers[0].score}
+    })
     
+    //Break entries into different arrays by date  
+    const diaryDatesNestedArray = []
+    diaryDatesArray.forEach((diaryCard) => {
+        diaryDatesNestedArray.push(diaryDatesArray.filter((diary_card) => diary_card.x === diaryCard.x))
+    })
+
+    const groupedDatesObject = {}
+    for (let i=0; i < diaryDatesArray.length; i++) {
+        const date = diaryDatesArray[i].x
+        if (!groupedDatesObject[date]) { groupedDatesObject[date] = [] }
+        groupedDatesObject[date].push(diaryDatesArray[i])
+    }
+
+    const uniqueDatesNestedArray = Object.values(groupedDatesObject).sort((a, b) => a[0].x > b[0].x ? 1 : -1)
+    
+    //Render one victory group per date
     function renderVictoryGroups() {
-
-        //Break entries into different arrays by date
-        // https://stackoverflow.com/questions/14696326/break-array-of-objects-into-separate-arrays-based-on-a-property
-        // https://www.google.com/search?ei=Qy83X-XhDYbEytMPyJKKgAQ&q=break+down+array+into+different+arrays+by+object+property&oq=break+down+array+into+different+arrays+by+object+property&gs_lcp=CgZwc3ktYWIQAzoECAAQRzoFCAAQzQJQ5bMDWPjBA2DewwNoA3AFeAGAAbEBiAHuB5IBBDE0LjGYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=psy-ab&ved=0ahUKEwilj67n_JvrAhUGonIEHUiJAkAQ4dUDCAw&uact=5
-
-        //Map a master array of date scores to render victory groups
-        console.log(user.diary_cards)
-        console.log(diaryDatesArray())
+        // return uniqueDatesNestedArray.map((data) => <UserVictoryGroup key={data[0].x} data={data} />)
+        return <UserVictoryGroup />
     }
 
     function renderChart() {
@@ -31,8 +41,6 @@ function UserTracker({ user }) {
             {/* Buffer date */}
             <VictoryGroup
                 color="#c43a31" 
-                labels={({ datum }) => `mood: ${datum.y}`}
-                labelComponent={ <VictoryTooltip style={{ fontSize: 16 }}/> }
                 data={[
                     { x: ' ', y: null },
                     // { x: '2020-08-03', y: 2 },
@@ -43,60 +51,16 @@ function UserTracker({ user }) {
                     // { x: '2020-08-05', y: 2 },
                     // { x: '2020-08-05', y: 0 }
                     ]}
-                    >
+                labels={({ datum }) => `mood: ${datum.y}`}
+                labelComponent={ <VictoryTooltip style={{ fontSize: 16 }}/> }
+                >
                 <VictoryLine/>
-                <VictoryScatter
-                size={({ active }) => active ? 8 : 5}
-                />
+                <VictoryScatter size={({ active }) => active ? 8 : 5}/>
                 </VictoryGroup>
 
-            {/* One group per day */}    
+            {/* One group per day */}   
             {renderVictoryGroups()}
 
-            {/* One group per day */}
-            <VictoryGroup
-                color="#c43a31" 
-                labels={({ datum }) => `mood: ${datum.y}`}
-                labelComponent={ <VictoryTooltip style={{ fontSize: 16 }}/> }
-                data={[
-                    // { x: '2020-07-31', y: 0 },
-                    { x: '2020-08-03', y: 2 },
-                    { x: '2020-08-03', y: 4 },
-                    { x: '2020-08-03', y: 5 },
-                    // { x: '2020-08-04', y: 3 },
-                    // { x: '2020-08-04', y: 4 },
-                    // { x: '2020-08-05', y: 2 },
-                    // { x: '2020-08-05', y: 1 }
-                    ]}
-                >
-            <VictoryLine/>
-            <VictoryScatter
-            size={({ active }) => active ? 8 : 5}
-            />
-            </VictoryGroup>
-
-            
-            {/* One group per day */}
-            <VictoryGroup
-                color="#c43a31" 
-                labels={({ datum }) => `mood: ${datum.y}`}
-                labelComponent={ <VictoryTooltip style={{ fontSize: 16 }}/> }
-                data={[
-                    // { x: '2020-07-31', y: 0 },
-                    // { x: '2020-08-03', y: 2 },
-                    // { x: '2020-08-03', y: 4 },
-                    // { x: '2020-08-03', y: 5 },
-                    // { x: '2020-08-04', y: 3 },
-                    // { x: '2020-08-04', y: 4 },
-                    { x: '2020-08-05', y: 2 },
-                    { x: '2020-08-05', y: 0 }
-                    ]}
-                    >
-                <VictoryLine/>
-                <VictoryScatter
-                size={({ active }) => active ? 8 : 5}
-                />
-            </VictoryGroup>
         </VictoryChart>
     </div>
     }
