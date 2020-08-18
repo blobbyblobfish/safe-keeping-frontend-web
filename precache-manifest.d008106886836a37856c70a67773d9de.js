a6cab662d009c939f2ae2787a7820525176114d5
blobbyblobfish <aywang6@gmail.com>
2020-08-18 12:02:19 -0400

    Updates

---
+++ precache-manifest.d008106886836a37856c70a67773d9de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92c0749c3d06633e02b189f58c657b7",
+    "url": "/safe-keeping-frontend-web/index.html"
+  },
+  {
+    "revision": "1a64fe57b407a13e190b",
+    "url": "/safe-keeping-frontend-web/static/css/main.6f4c0804.chunk.css"
+  },
+  {
+    "revision": "007b31ae676d4b567692",
+    "url": "/safe-keeping-frontend-web/static/js/2.05e2efe9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/safe-keeping-frontend-web/static/js/2.05e2efe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a64fe57b407a13e190b",
+    "url": "/safe-keeping-frontend-web/static/js/main.542da1f2.chunk.js"
+  },
+  {
+    "revision": "708b8bece13f5c71a800",
+    "url": "/safe-keeping-frontend-web/static/js/runtime-main.e32d5e49.js"
+  }
+]);
